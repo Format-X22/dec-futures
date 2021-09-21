ede99dc837eb8eb6ef2aefce6b086ad411aef8b8
oleg <oleg.pav.m@gmail.com>
2021-09-21 19:32:43 +0300

    DxDy full loader.

--- apps/aggregator/src/market/dxdy.service.ts
+++ apps/aggregator/src/market/dxdy.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { EMarketKey } from '@app/shared/entity.enum';
-import * as sleep from 'sleep-promise';
 import { DydxClient } from '@dydxprotocol/v3-client';
 import * as moment from 'moment';
 import { AbstractMarketService } from './abstract-market.service';
+import { Funding } from '@app/shared/funding.schema';
+import { DydxMarket } from '@dydxprotocol/starkex-lib';
+import * as sleep from 'sleep-promise';
 
 // Fix Web3 types definition error in commonjs mode
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Web3 = require('web3');
-
-type TRawHistoryResponse = Array<{
-    rate: string;
-    effectiveAt: string;
-}>;
 
 type THistory = Array<{
     rate: number;
@@ -20,6 +17,7 @@ type THistory = Array<{
 }>;
 
 const API = 'https://api.dydx.exchange';
+const API_CALL_DELAY = 1000;
 
 @Injectable()
 export class DxDyService extends AbstractMarketService {
@@ -40,32 +38,106 @@ export class DxDyService extends AbstractMarketService {
             lastDate = this.makeEmptyHistoryDeepDate();
         }
 
-        await this.syncToLastKnownDate(lastDate);
+        for (const pair of await this.getPairs()) {
+            await this.syncToLastKnownDate(pair, lastDate);
+            await sleep(API_CALL_DELAY);
+        }
     }
 
     private async getLastFundingDate(): Promise<Date | null> {
-        // TODO -
-        return;
+        const last: Funding | null = await this.fundingModel.findOne(
+            { marketKey: this.name },
+            { payDate: 1 },
+            { sort: { payDate: -1 } },
+        );
+
+        return last?.payDate || null;
     }
 
     private makeEmptyHistoryDeepDate(): Date {
         return moment().subtract(1, 'month').toDate();
     }
 
-    private async syncToLastKnownDate(date: Date): Promise<void> {
-        for (const pair of await this.getPairs()) {
-            // TODO Loop with sleep
-            // TODO Save
+    private async syncToLastKnownDate(pair: DydxMarket, date: Date): Promise<void> {
+        const history: THistory = [];
+        let lastDate: Date | null = null;
+
+        const currentMarketData = await this.client.public.getMarkets(pair);
+        const pairData = currentMarketData.markets[pair];
+
+        history.push({
+            rate: Number(pairData.nextFundingRate),
+            payDate: new Date(pairData.nextFundingAt),
+        });
+
+        while (true) {
+            const currentHistory = await this.getFundingHistory(pair, lastDate);
+
+            if (!currentHistory.length) {
+                await this.saveHistory(pair, history);
+                break;
+            }
+
+            const isHistoryContainsNotOnlyNextFundingData = history.length > 1;
+
+            if (isHistoryContainsNotOnlyNextFundingData) {
+                history.pop();
+            }
+
+            history.push(...currentHistory);
+            lastDate = currentHistory[currentHistory.length - 1].payDate;
+
+            if (lastDate <= date) {
+                await this.saveHistory(pair, history);
+                break;
+            }
+
+            await sleep(API_CALL_DELAY);
         }
     }
 
-    private async getPairs(): Promise<Array<string>> {
-        // TODO -
-        return [];
+    private async saveHistory(pair: DydxMarket, history: THistory): Promise<void> {
+        for (const item of history) {
+            const [base, quote] = pair.split('-');
+
+            await this.fundingModel.updateOne(
+                {
+                    payDate: item.payDate,
+                },
+                {
+                    $set: {
+                        rate: item.rate,
+                        payDate: item.payDate,
+                        marketKey: EMarketKey.DXDY,
+                        base,
+                        quote,
+                    },
+                },
+                {
+                    upsert: true,
+                },
+            );
+        }
     }
 
-    private async getFundingHistory(pair: string, beforeDate: Date | null): Promise<THistory> {
-        // TODO -
-        return;
+    private async getPairs(): Promise<Array<DydxMarket>> {
+        const response = await this.client.public.getMarkets();
+
+        return Object.keys(response.markets) as Array<DydxMarket>;
+    }
+
+    private async getFundingHistory(pair: DydxMarket, beforeDate: Date | null): Promise<THistory> {
+        const options: Parameters<DydxClient['public']['getHistoricalFunding']>[0] = { market: pair };
+
+        if (beforeDate) {
+            options.effectiveBeforeOrAt = beforeDate.toISOString();
+        }
+
+        const result = await this.client.public.getHistoricalFunding(options);
+
+        return result.historicalFunding.map((item) => ({
+            rate: Number(item.rate),
+            payDate: new Date(item.effectiveAt),
+        }));
     }
 }
